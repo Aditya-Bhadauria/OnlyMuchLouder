5a1347c712a84f4514dc38da2412285e0e3420c0
AdityaBhadauria <adityab5262@gmail.com>
2024-10-27 10:23:42 +0530

    Removed old files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
+
 import viteLogo from '/vite.svg'
 import './App.css'
 // import Cursor from './components/Cursor'
 
-import LandingPage from './Pages/Landingpage'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,7 +12,7 @@ function App() {
   return (
     <>
       {/* <Cursor/> */}
-      <LandingPage/>
+      
     </>
   )
 }
